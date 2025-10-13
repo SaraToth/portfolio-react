98ce270b98f8b16eefee21302efc2824a0dedf43
SaraToth <saratoth101@gmail.com>
2025-10-14 08:52:28 +0900

    Feat: Create basic projects component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,8 +1,30 @@
 const Works = () => {
         return (
-        <section>
-            this is the works section
-        </section>
+            <section id="works" className="home-works">
+                <div className="container">
+                    <h2>Works</h2>
+                    <div className="works__container">
+
+                    {/* Individual projects create dynamiclly through iteration */}
+                            <div className="works__work">
+                                <img 
+                                    src=""
+                                    alt="Project Image"
+                                    width="300"
+                                    height="300"
+                                    loading="lazy"
+                                    class="home-projects__img"
+                                />
+                                <div class="home-projects__content-container">
+                                    <h3 class="home-projects__h3">Project Name</h3>
+                                    <div class="home-projects__content">Project Description</div>
+                                    <a href="./project-${projectNumber}" class="btn-link" alt="">View Project Name</a>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+            </section>
     )
 };
 
