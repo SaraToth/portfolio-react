f8776ef821c806e75d4d2bd5744fee78517006e3
SaraToth <saratoth101@gmail.com>
2025-10-14 08:46:50 +0900

    Feat: Create about component content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,38 @@
 const About = () => {
     return(
-        <section>
-            This is the about section
+        <section id="about" className="home-about">
+            <div className="container">
+                <h2>About Me</h2>
+                <div>
+                    <div>
+                        <h3>Hello,</h3>
+                        <p>I'm a passionate Full Stack Developer building and managing the Front-end and Back-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
+                    </div>
+                    <div>
+                        <h3>My Skills,</h3>
+                        <div>
+                            <div>HTML</div>
+                            <div>CSS</div>
+                            <div>Javascript</div>
+                            <div>React</div>
+                            <div>Git</div>
+                            <div>Github</div>
+                            <div>Node.js</div>
+                            <div>Express.js</div>
+                            <div>Typescript</div>
+                            <div>SQL</div>
+                            <div>PostgreSQL</div>
+                            <div>Prisma</div>
+                            <div>Webpack</div>
+                            <div>Jest</div>
+                            <div>Responsive Design</div>
+                            <div>Accesibility (a11y)</div>
+                            <div>REST API</div>
+                            <div>Deployment</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 };
