5551b5e29335a7d268f5d60fdafa98e8bef3a6ef
SaraToth <saratoth101@gmail.com>
2025-10-14 13:58:07 +0900

    Feat: Create hero content for project pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,33 @@
 import { Link } from "react-router-dom"
 
-const Hero = () => {
+const Hero = ({page}) => {
 
-    return(
-        <section>
-            <div className="container">
-                <h1>Sara Toth</h1>
-                <h2>Fullstack Developer</h2>
-                <div className="content">
-                    <p>A Result-Oriented Web Developer building and managing Websites and Web Applications that leads to the success of the overall product</p>
+    if (page === "home") {
+        return (
+            <section id="hero" className="home-hero">
+                <div className="container">
+                    <h1>Sara Toth</h1>
+                    <h2>Fullstack Developer</h2>
+                    <div className="content">
+                        <p>A Result-Oriented Web Developer building and managing Websites and Web Applications that leads to the success of the overall product</p>
+                    </div>
+                    <Link to="/#works" >Works</Link>
                 </div>
-                <Link to="/#works" >Works</Link>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    } else if (page === "project") {
+        return(
+            <section id="hero" className="project-hero">
+                <div className="container">
+                    <h1>Project Name</h1>
+                    <div className="content">
+                        <p>Project Description</p>
+                        <Link to="Live Link" target="_blank" rel="noopener noreferrer">Live Link</Link>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 };
 
 export default Hero;
