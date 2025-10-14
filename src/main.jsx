a367caeadcd756811187102c4844d84c8de01bb9
SaraToth <saratoth101@gmail.com>
2025-10-14 13:57:48 +0900

    Fix: Routes to match projects/projectId

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <App/>,
     children: [
       { index: true, element: <Home/> },
-      { path: "works/:workId", element: <Project/> }
+      { path: "projects/:projectId", element: <Project/> }
     ]
   },
 ]);
