3bb197729d409b460132370a8191df8e2c400ddb
SaraToth <saratoth101@gmail.com>
2025-10-14 14:38:38 +0900

    Feat: Finish conditional rendering about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,40 +1,73 @@
-const About = () => {
-    return(
-        <section id="about" className="home-about">
-            <div className="container">
-                <h2>About Me</h2>
-                <div>
+const About = ({page}) => {
+
+    if (page === "home") {
+        return(
+            <section id="about" className="home-about">
+                <div className="container">
+                    <h2>About Me</h2>
                     <div>
-                        <h3>Hello,</h3>
-                        <p>I'm a passionate Full Stack Developer building and managing the Front-end and Back-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
-                    </div>
-                    <div>
-                        <h3>My Skills,</h3>
                         <div>
-                            <div>HTML</div>
-                            <div>CSS</div>
-                            <div>Javascript</div>
-                            <div>React</div>
-                            <div>Git</div>
-                            <div>Github</div>
-                            <div>Node.js</div>
-                            <div>Express.js</div>
-                            <div>Typescript</div>
-                            <div>SQL</div>
-                            <div>PostgreSQL</div>
-                            <div>Prisma</div>
-                            <div>Webpack</div>
-                            <div>Jest</div>
-                            <div>Responsive Design</div>
-                            <div>Accesibility (a11y)</div>
-                            <div>REST API</div>
-                            <div>Deployment</div>
+                            <h3>Hello,</h3>
+                            <p>I'm a passionate Full Stack Developer building and managing the Front-end and Back-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
+                        </div>
+                        <div>
+                            <h3>My Skills,</h3>
+                            <div>
+                                <div>HTML</div>
+                                <div>CSS</div>
+                                <div>Javascript</div>
+                                <div>React</div>
+                                <div>Git</div>
+                                <div>Github</div>
+                                <div>Node.js</div>
+                                <div>Express.js</div>
+                                <div>Typescript</div>
+                                <div>SQL</div>
+                                <div>PostgreSQL</div>
+                                <div>Prisma</div>
+                                <div>Webpack</div>
+                                <div>Jest</div>
+                                <div>Responsive Design</div>
+                                <div>Accesibility (a11y)</div>
+                                <div>REST API</div>
+                                <div>Deployment</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    } else if (page === "project") {
+        return(
+            <section id="about" className="project-about">
+                <div className="container">
+                    <img 
+                        src="null"
+                        alt="Project Image"
+                        width="300"
+                        height="300"
+                        loading="lazy"
+                        className="project-about__img"
+                    />
+                    <h2>Project Overview</h2>
+                    <div>
+                        <p>Project Overview</p></div>
+                    <h3>Tools Used</h3>
+                    <div>
+                        <div>HTML</div>
+                        <div>CSS</div>
+                        <div>Javascript</div>
+                    </div>
+                    <h3>See Live</h3>
+                    <div>
+                        <a href="targetLink" target="_blank" rel="noopener noreferrer">Live Link</a>
+                        <a href="codeLink" target="_blank" rel="noopener noreferrer">Code Link</a>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
 };
 
 export default About;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,21 +6,23 @@ const Projects = () => {
                 <div className="projects__container">
 
                 {/* Individual projects create dynamiclly through iteration */}
-                        <div className="projects__project">
-                            <img 
-                                src="null"
-                                alt="Project Image"
-                                width="300"
-                                height="300"
-                                loading="lazy"
-                                className="home-projects__img"
-                            />
-                            <div className="home-projects__content-container">
-                                <h3 className="home-projects__h3">Project Name</h3>
-                                <div className="home-projects__content">Project Description</div>
-                                <a href="./project-${projectNumber}" className="btn-link" alt="">View Project Name</a>
+                    <div className="projects__project">
+                        <img 
+                            src="null"
+                            alt="Project Image"
+                            width="300"
+                            height="300"
+                            loading="lazy"
+                            className="home-projects__img"
+                        />
+                        <div className="home-projects__content-container">
+                            <h3>Project Name</h3>
+                            <div>
+                                <p>Project Description</p>
                             </div>
+                            <a href="./project-${projectNumber}" className="btn-link" alt="">View Project Name</a>
                         </div>
+                    </div>
                 </div>
             </div>
         </section>
