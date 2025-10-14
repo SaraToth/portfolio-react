ceaf27c9395daec17168db16f52df37775afdcc6
SaraToth <saratoth101@gmail.com>
2025-10-15 08:59:24 +0900

    Feat: Finish Header content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 function App() {
+  const { pathname } = useLocation();
+  const isProjectPage = pathname.startsWith("/projects");
+
   return (
     <>
-    <Header/>
+    <Header isProjectPage={isProjectPage}/>
     <Outlet/>
     <Footer/>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,54 +1,55 @@
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({isProjectPage}) => {
 
-    const hamburger = false;
+    // Default pageVariant is Home
+    const pageVariant = isProjectPage ? "/" : "";
 
-    if (!hamburger) {
-        return (
-            <header>
-                {/* Left: Logo */}
-                <div className="header__logo"><a href="/#home">Sara Toth</a></div>
+    // Default for Home
+    const headerLinks = [
+        { link: "#", text: "Home" },
+        { link: "#about", text: "About" },
+        { link: "#projects", text: "Work" },
+        { link: "#contact", text: "Contact" }
+    ];
 
-                {/* Center: Navigation Links */}
-                <nav>
-                    <ul>
-                        <li><Link to="/">Sara Toth</Link></li>
-                        <li><Link to="/#" >Home</Link></li>
-                        <li><Link to="/#about" >About</Link></li>
-                        <li><Link to="/#works" >Works</Link></li>
-                        <li><Link to="/#contact" >Contact</Link></li>
-                    </ul>
-                </nav>
+    // Update nav links per page
+    const nav = (
+        <nav>
+            <ul>
+                {headerLinks.map((link) => (
+                    // Need a forward slash
+                    <li key={link.text}><Link to={ pageVariant + link.link}>{link.text}</Link></li>
+                ))}
+            </ul>
+        </nav>
+    );
 
-                {/* Right: Language button */}
-                <Link to="/" className="language" data-langauge="kr" >KR</Link>
-            </header>
-        )
-    } else if (hamburger){
-        return(
-            <header className="header">
-                {/* Left: Logo */}
-                <div className="header__logo"><a href="/#home">Sara Toth</a></div>
+    return (
+        <header>
+            {/* Left: Logo */}
+            <Link to="/">Sara Toth</Link>
 
-                {/* Right: Language button */}
-                <Link to="/" className="language" data-langauge="kr" >KR</Link>
+            {/* Center: Navigation Links */}
+            {nav}
 
-                {/* Far Right: Hamburger Menu */}
-                <button aria-label="Menu" className="header__hamburger-open">&#9776;</button>
-                <button className="header__hamburger-close">&#x2715;</button>
+            {/* Right: Language button */}
+            <Link to="/" className="language" data-langauge="kr" >KR</Link>
 
-                {/* Dropdown menu that appears on small screens */}
-                <div>
-                    <Link to="/#" >Home</Link>
-                    <Link to="/#about" >About</Link>
-                    <Link to="/#works" >Works</Link>
-                    <Link to="/#contact" >Contact</Link>
-                </div>
+            {/* Far Right: Hamburger Menu */}
+            <button aria-label="Menu" className="header__hamburger-open">&#9776;</button>
+            <button className="header__hamburger-close">&#x2715;</button>
 
-            </header>
-        )
-    }
+            {/* Dropdown menu that appears on small screens */}
+            <div>
+                <Link to="/#">Home</Link>
+                <Link to="/#about" >About</Link>
+                <Link to="/#works" >Works</Link>
+                <Link to="/#contact" >Contact</Link>
+            </div>
+
+        </header>
+    )
 };
 
 export default Header;
