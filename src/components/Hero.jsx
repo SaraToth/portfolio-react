2d11ed6f115ffb6df66adad08ffc51a4286d2765
SaraToth <saratoth101@gmail.com>
2025-10-16 08:59:37 +0900

    Feat: Rename projects to numbers vs words

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Hero = ({page}) => {
+
+    const { projectId } = useParams();
+    const { t } = useTranslation();
 
     if (page === "home") {
         return (
             <section id="hero" className="home-hero">
                 <div className="container">
-                    <h1>Sara Toth</h1>
-                    <h2>Fullstack Developer</h2>
+                    <h1>{t("home.hero.h1")}</h1>
+                    <h2>{t("home.hero.h2")}</h2>
                     <div className="content">
-                        <p>A Result-Oriented Web Developer building and managing Websites and Web Applications that leads to the success of the overall product</p>
+                        <p>{t("home.hero.content")}</p>
                     </div>
                     <Link to="/#works" >Works</Link>
                 </div>
             </section>
         )
     } else if (page === "project") {
+        const project = t(`projects.${projectId}`, { returnObjects: true });
+
         return(
             <section id="hero" className="project-hero">
                 <div className="container">
-                    <h1>Project Name</h1>
+                    <h1>{project.name}</h1>
                     <div className="content">
-                        <p>Project Description</p>
+                        <p>{project.brief}</p>
                         <Link to="Live Link" target="_blank" rel="noopener noreferrer">Live Link</Link>
                     </div>
                 </div>
