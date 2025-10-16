fe0aa33f769fee77c68bf7788248b5889604af21
SaraToth <saratoth101@gmail.com>
2025-10-16 22:02:03 +0900

    Feat: Implement lazy load pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
+import React, { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./styles/globals.css";
 import App from './App.jsx'
-import Home from "./pages/Home.jsx"
-import Project from "./pages/Project.jsx"
+// import Home from "./pages/Home.jsx"
+// import Project from "./pages/Project.jsx"
 import "./i18n";
+
+// Lazy Imports
+const Home = React.lazy(() => import("./pages/Home.jsx"));
+const Project = React.lazy(() => import("./pages/Project.jsx"));
 
 
 const router = createBrowserRouter([
@@ -14,13 +18,15 @@ const router = createBrowserRouter([
     element: <App/>,
     children: [
       { index: true, element: <Home/> },
-      { path: "projects/:projectId", element: <Project/> }
+      { path: "projects/:projectId", element: <Project/>}
     ]
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
     <RouterProvider router={router} />
+    </Suspense>
   </StrictMode>,
 )
