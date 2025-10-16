a30697ac559e62f75f52060652dc85ca21d69b23
SaraToth <saratoth101@gmail.com>
2025-10-16 13:59:36 +0900

    Feat: Finish projects page about i18n logic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
 import { useTranslation } from "react-i18next";
+import { useParams, Link } from "react-router-dom";
 
 const About = ({page}) => {
 
+    const { projectId } = useParams();
     const { t } = useTranslation();
 
     if (page === "home") {
@@ -42,30 +44,39 @@ const About = ({page}) => {
             </section>
         )
     } else if (page === "project") {
+        const project = t(`projects.${projectId}`, { returnObjects: true });
+
         return(
             <section id="about" className="project-about">
                 <div className="container">
                     <img 
-                        src="null"
+                        src={project.imgSrc || "null"}
                         alt="Project Image"
                         width="300"
                         height="300"
                         loading="lazy"
-                        className="project-about__img"
                     />
                     <h2>Project Overview</h2>
                     <div>
-                        <p>Project Overview</p></div>
+                        <p>{project.overview}</p></div>
                     <h3>Tools Used</h3>
                     <div>
-                        <div>HTML</div>
-                        <div>CSS</div>
-                        <div>Javascript</div>
+                        {project.tools.map((tool) => (
+                            <div key={tool}>{tool}</div>
+                        ))}
                     </div>
                     <h3>See Live</h3>
                     <div>
-                        <a href="targetLink" target="_blank" rel="noopener noreferrer">Live Link</a>
-                        <a href="codeLink" target="_blank" rel="noopener noreferrer">Code Link</a>
+                        <Link 
+                            to={project.liveLink}
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >Live Link</Link>
+                        <Link
+                            to={project.codeLink}
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >Code Link</Link>
                     </div>
                 </div>
             </section>
