dfe78b6d11f5cf38c159f5902c2925fe760c0558
SaraToth <saratoth101@gmail.com>
2025-10-16 17:05:27 +0900

    Feat: Style footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import { Outlet, useLocation } from "react-router-dom";
 
 function App() {
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>&#169; 2025. Sara Toth. All rights reserved.</p>
+        </footer>
+    )
+};
+
+export default Footer;
